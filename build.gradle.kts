78c402bd085df91d92f8815502e747e8f6108a86
radekzapletal803-lab <radekzapletal803@gmail.com>
2026-07-25 07:32:24 +0200

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+}
+
+group = "cz.radek.advancedwelcome"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+
+    processResources {
+        filteringCharset = "UTF-8"
+    }
+}
